e5271c36080d983c372543b7407901db70719298
anuja2015 <armdevu15@gmail.com>
2024-12-24 14:29:42 +0530

    Update app.js

--- samplenodejs/app.js
+++ samplenodejs/app.js
@@ -4,13 +4,11 @@ const express = require('express');
 // Create an instance of an Express app
 const app = express();
 
+app.use(express.static('public'))
+
 // Define a port for the server
 const PORT = 3000;
 
-// Define a route for the home page
-app.get('/', (req, res) => {
-    res.send('<h1>This is a sample Node.js project</h1>');
-});
 
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
