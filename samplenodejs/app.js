7924e1c2efc9acbc43279392594b8e1161a9feb9
anuja2015 <armdevu15@gmail.com>
2025-03-01 16:41:40 +0530

    pipelines copy

--- samplenodejs/app.js
+++ samplenodejs/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'))
 
 // Define a port for the server
-const PORT = 3010;
+const PORT = process.env.PORT | 8080;
 
 
 // Start the server and listen on the defined port
